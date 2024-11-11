20619b541b77b787f73aa384239f8d7a95c7ee5d
Samuel Vaudo <samuel.vaudo@mymail.champlain.edu>
2024-11-11 14:05:58 -0500

    submit

--- week12/apacheLogMenu.bash
+++ week12/apacheLogMenu.bash
@@ -1,6 +1,8 @@
-#! /bin/bash
+suspiciousVisitors#! /bin/bash
 
 logFile="/var/log/apache2/access.log.1"
+
+iocFile="ioc.txt"
 
 function displayAllLogs(){
 	cat "$logFile"
@@ -10,15 +12,15 @@ function displayOnlyIPs(){
         cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
 }
 
-# function: displayOnlyPages:
-# like displayOnlyIPs - but only pages
-
+function displayOnlyPages(){
+	cat "$logFile" | cut -d ' ' -f 7 | sort -n | uniq -c
+}
 function histogram(){
 
 	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
                               | uniq)
 	# This is for debugging, print here to see what it does to continue:
-	# echo "$visitsPerDay"
+	echo "$visitsPerDay"
 
         :> newtemp.txt  # what :> does is in slides
 	echo "$visitsPerDay" | while read -r line;
@@ -26,22 +28,35 @@ function histogram(){
 		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
                                      | cut -d ":" -f 1)
 		local IP=$(echo "$line" | cut -d  " " -f 1)
-          
 		local newLine="$IP $withoutHours"
 		echo "$IP $withoutHours" >> newtemp.txt
-	done 
+	done
 	cat "newtemp.txt" | sort -n | uniq -c
 }
 
-# function: frequentVisitors: 
-# Only display the IPs that have more than 10 visits
-# You can either call histogram and process the results,
-# Or make a whole new function. Do not forget to separate the 
-# number and check with a condition whether it is greater than 10
-# the output should be almost identical to histogram
-# only with daily number of visits that are greater than 10 
+function frequentVisitors(){
+	#used histogram func but added an awk argument for if the number of visits is greater than or equal to 10
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
+                              | uniq)
+        :> newtemp.txt  # what :> does is in slides
+        echo "$visitsPerDay" | while read -r line;
+        do
+                local withoutHours=$(echo "$line" | cut -d " " -f 2 \
+                                     | cut -d ":" -f 1)
+                local IP=$(echo "$line" | cut -d  " " -f 1)
+                local newLine="$IP $withoutHours"
+                echo "$IP $withoutHours" >> newtemp.txt
+        done
+        cat "newtemp.txt" | sort -n | uniq -c | awk '{ if ($1 > 9) print $1 " " $2 " " $3 }'
+} 
 
 # function: suspiciousVisitors
+
+function suspiciousVisitors(){
+	local visitsWithIndicators=$(cat "$logFile" | cut -d " " -f 4,7 | tr -d '['  | sort \
+                              | uniq)
+	echo "$visitsWithIndicators" | awk '{ if (grep -q $2 $iocFile ) print $1 " " $2 }'
+}
 # Manually make a list of indicators of attack (ioc.txt)
 # filter the records with this indicators of attack
 # only display the unique count of IP addresses.  
@@ -56,17 +71,17 @@ do
 	echo "PLease select an option:"
 	echo "[1] Display all Logs"
 	echo "[2] Display only IPS"
-	# Display only pages visited
+	echo "[3] Display only Pages"
 	echo "[4] Histogram"
-	# Frequent visitors
-	# Suspicious visitors
+	echo "[5] Display frequent visitors"
+	echo "[6] Display suspicous visitors"
 	echo "[7] Quit"
 
 	read userInput
 	echo ""
 
 	if [[ "$userInput" == "7" ]]; then
-		echo "Goodbye"		
+		echo "Goodbye"
 		break
 
 	elif [[ "$userInput" == "1" ]]; then
@@ -77,15 +92,21 @@ do
 		echo "Displaying only IPS:"
 		displayOnlyIPs
 
-	# Display only pages visited
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying only Pages"
+		displayOnlyPages
 
 	elif [[ "$userInput" == "4" ]]; then
 		echo "Histogram:"
 		histogram
 
-        # Display frequent visitors
-	# Display suspicious visitors
-	# Display a message, if an invalid input is given
+	elif [[ "$userInput" == "5" ]]; then
+                echo "Displaying frequent Visitors"
+               frequentVisitors
+
+
+	elif [[ "$userInput" == "6" ]]; then
+                echo "Displaying Suspicious Visitors"
+                suspiciousVisitors
 	fi
 done
-
